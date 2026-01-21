0a46bf622d0ba5309ca620be7a755e58147b67f3
rhtnddd <qlalfql123@gmail.com>
2026-01-21 17:30:26 +0900

    Basic configuration

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import { HomeMobile, HomeWeb } from './pages/Home'
 import { useMediaQuery } from './hooks/useMediaQuery'
 import './App.css'
-
 function HomePage() {
   const isMobile = useMediaQuery('(max-width: 767px)')
   return isMobile ? <HomeMobile /> : <HomeWeb />
