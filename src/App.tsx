4db3f87817d9ce7cfd784e951d988f5a1ff1bba3
rhtnddd <qlalfql123@gmail.com>
2026-01-23 17:47:18 +0900

    Login and SignUp function

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import { HomeMobile, HomeWeb } from './pages/Home'
+import { LoginMobile, LoginWeb } from './pages/Login'
+import { SignUpMobile, SignUpWeb } from './pages/SignUp'
 import { useMediaQuery } from './hooks/useMediaQuery'
 import './App.css'
+function LoginPage() {
+  const isMobile = useMediaQuery('(max-width: 767px)')
+  return isMobile ? <LoginMobile /> : <LoginWeb />
+}
+function SignUpPage() {
+  const isMobile = useMediaQuery('(max-width: 767px)')
+  return isMobile ? <SignUpMobile /> : <SignUpWeb />
+}
 function HomePage() {
   const isMobile = useMediaQuery('(max-width: 767px)')
   return isMobile ? <HomeMobile /> : <HomeWeb />
@@ -10,6 +20,8 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
         <Route path="/home" element={<HomePage />} />
       </Routes>
     </BrowserRouter>
