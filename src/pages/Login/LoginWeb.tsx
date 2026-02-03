74ca12a4aca39880fe71f2e6924aa1138aff478e
rhtnddd <qlalfql123@gmail.com>
2026-02-03 23:55:49 +0900

    CSS Add

--- src/pages/Login/LoginWeb.tsx
+++ src/pages/Login/LoginWeb.tsx
@@ -1,8 +1,9 @@
 import { useState, useEffect } from 'react';
+import styles from './LoginWeb.module.css'
 export const LoginWeb = () => {
   return (
     <div>
-        <h1>돌아오신 것<br></br>을 환영합니다</h1>
+        <h1 className={styles.logintext}>돌아오신 것<br></br>을 환영합니다</h1>
     </div>
   )
 }
