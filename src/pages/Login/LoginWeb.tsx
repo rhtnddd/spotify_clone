103f3281130f8517b7058104188af703b81b910b
rhtnddd <qlalfql123@gmail.com>
2026-02-06 23:52:49 +0900

    logo

--- src/pages/Login/LoginWeb.tsx
+++ src/pages/Login/LoginWeb.tsx
@@ -1,8 +1,15 @@
 import { useState, useEffect } from 'react';
+import spotifyLogo from '../../assets/spotify_logo.svg'
 import styles from './LoginWeb.module.css'
 export const LoginWeb = () => {
   return (
     <div>
+        <img
+          src={spotifyLogo}
+          alt="Spotify"
+          className={styles.logo}
+        >
+        </img>
         <h1 className={styles.logintext}>돌아오신 것<br></br>을 환영합니다</h1>
     </div>
   )
